80c78aa4e01e5c7ca2f07c1b06724e79c24cb109
Veronika Ten <verona.binko@gmail.com>
2023-01-10 12:35:19 +0300

    feat: add filter, info, searchPanel layout in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Filter from "./components/filter/Filter";
+import Info from "./components/info/Info";
+import SearchPanel from "./components/searchPanel/SearchPanel";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Info />
+      <div className="search-panel">
+        <SearchPanel />
+        <Filter />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/info/Info.tsx
@@ -0,0 +1,13 @@
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting for employees in the Diamond company</h1>
+      <h2>Total number of employees: </h2>
+      <h2>The award will be received: </h2>
+    </div>
+  );
+};
+
+export default Info;
