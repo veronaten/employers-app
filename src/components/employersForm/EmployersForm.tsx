00c1be67dc62b74008940fdc23cdef47ec3aa558
Veronika Ten <verona.binko@gmail.com>
2023-01-29 23:54:49 +0300

    feat: rewrite employersForm component to class

--- src/components/employersForm/EmployersForm.tsx
+++ src/components/employersForm/EmployersForm.tsx
@@ -1,27 +1,57 @@
+import React from "react";
 import "./EmployersForm.css";
 
-const EmployersForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employee</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="What is his name?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Salary in $?"
-        />
+interface EmployersFormState {
+  name: string;
+  salary: string;
+}
+class EmployersForm extends React.Component<{}, EmployersFormState> {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  );
-};
+  inputChangeHandler = (e: {
+    preventDefault: () => void;
+    target: { value: any };
+  }) => {
+    e.preventDefault();
+    this.setState({
+      name: e.target.value,
+      salary: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employee</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="What is his name?"
+            onChange={this.inputChangeHandler}
+            value={name}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Salary in $?"
+            value={salary}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployersForm;
